e1c6a075118efe01cb820808f0fc793621a55cc6
hungthinhphung <hungthinhphung1996@gmail.com>
2020-03-15 19:57:55 +0700

    Fix write html

--- EbookUtility/src/ebook.py
+++ EbookUtility/src/ebook.py
@@ -29,4 +29,29 @@ class Ebook:
     def to_html(self):
         if not self.is_valid():
             raise Exception('Invalid ebook')
+        with open('../data/{}.html'.format(self.title), 'w', encoding='utf-8') as f:
+            f.write(self.__render_html())
 
+    def __render_html(self):
+        chapter_num = len(self.chapter)
+        content = ''
+        tob = ''
+        for i in range(chapter_num):
+            content += self.render_chapter_data(self.chapter[i], self.data[i], i)
+            tob += self.render_chapter_title(self.chapter[i], i)
+
+        header = '<!doctype html><html><head><meta charset="utf-8">'\
+                 '<title>{}</title></head><body>{}{}</body></html>'
+        title = self.title.replace('-', ' ').title()
+        return header.format(title, tob, content)
+
+    @staticmethod
+    def render_chapter_data(chapter, data, id):
+        result = '<h2 id="{}">{}</h2><br>'.format(id + 1, chapter)
+        for p in data:
+            result += '<br><p>{}</p>'.format(p)
+        return result
+
+    @staticmethod
+    def render_chapter_title(chapter, id):
+        return '<a href="#{}">{}</a><br>'.format(id + 1, chapter)
